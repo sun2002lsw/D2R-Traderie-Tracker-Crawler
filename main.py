6263982326718d4edc31cc2b683ac4b439ec354f
Sungwook Lee <limit_speed@naver.com>
2025-08-30 23:13:55 +0900

    Deploy to cloud run

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import app
 
 # 단순히 cloud run 에서 실행하기 위해 8080 포트 listen
 flask_app = Flask(__name__)
-flask_app.run(host='0.0.0.0', port=8080, debug=False)
+flask_app.run(host="0.0.0.0", port=8080, debug=False)
 
 # 실제 크롤링 서비스
 app.run()
