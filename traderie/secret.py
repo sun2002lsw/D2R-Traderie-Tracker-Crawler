adbc44053676d09c32c1af556256b7de02d49f40
Sungwook Lee <limit_speed@naver.com>
2025-08-31 17:17:36 +0900

    Add linter

--- traderie/secret.py
+++ traderie/secret.py
@@ -5,17 +5,17 @@ from google.cloud import secretmanager
 def get_traderie_credentials():
     traderie_id = os.environ.get("TRADERIE_ID")
     traderie_pwd = os.environ.get("TRADERIE_PWD")
-    
+
     if not traderie_id or not traderie_pwd:
         project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
         if not project_id:
             raise ValueError("GOOGLE_CLOUD_PROJECT 환경변수가 설정되지 않았습니다")
-            
+
         if not traderie_id:
             traderie_id = _get_gcp_secret(project_id, "TRADERIE_ID")
         if not traderie_pwd:
             traderie_pwd = _get_gcp_secret(project_id, "TRADERIE_PWD")
-    
+
     return traderie_id, traderie_pwd
 
 
